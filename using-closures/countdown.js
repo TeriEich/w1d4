ee06891bd0cc1b190f612237022b9535a359679a
Teri <teri.eich@gmail.com>
2018-11-29 22:33:02 +0000

    created countdown generator

--- using-closures/countdown.js
+++ using-closures/countdown.js
@@ -1,5 +1,16 @@
 var countdownGenerator = function (x) {
   /* your code here */
+
+  return function() {
+    if (x > 0) {
+      console.log(`T-minus ${x}...`);
+    } else if (x < 0) {
+      console.log(`Rockets already gone, bub!`);
+    } else {
+      console.log(`Blast Off!`);
+    }
+    x -= 1;
+  }
 };
 
 var countdown = countdownGenerator(3);
@@ -12,3 +23,9 @@ countdown(); // Rockets already gone, bub!
 
 
 //takes in a number x and returns a function that counts down when it is called
+
+//get number
+//use counter starting at number
+//closure: print out the results
+//if statements: +num = t-mins, 0 = blast off, -num = rocket out;
+//counter--
